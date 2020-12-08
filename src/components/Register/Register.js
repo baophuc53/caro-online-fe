e53b2eb77598a85ba8372178c68c09a2a9ee093e
kyquan <quanky99@gmail.com>
2020-12-08 19:12:31 +0700

    create UI for login and register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -17,16 +17,14 @@ import {
 import { QuestionCircleOutlined } from "@ant-design/icons";
 import Axios from "axios";
 
-
-
 const formItemLayout = {
   labelCol: {
     xs: { span: 24 },
-    sm: { span: 5 },
+    sm: { span: 10 },
   },
   wrapperCol: {
     xs: { span: 24 },
-    sm: { span: 16 },
+    sm: { span: 14 },
   },
 };
 const tailFormItemLayout = {
@@ -36,8 +34,8 @@ const tailFormItemLayout = {
       offset: 0,
     },
     sm: {
-      span: 16,
-      offset: 4,
+      span: 10,
+      offset: 15,
     },
   },
 };
@@ -46,7 +44,7 @@ const RegistrationForm = () => {
   const [form] = Form.useForm();
 
   const onFinish = (values) => {
-    const {username, password, nickname} = values;
+    const { username, password, nickname } = values;
     Axios.put(`${config.dev.path}/user`, { username, password, nickname })
       .then((res) => {
         console.log(res);
@@ -61,94 +59,128 @@ const RegistrationForm = () => {
   };
 
   return (
-    <div className="form-register">
-      <h2>REGISTER</h2>
-      <Form
-        {...formItemLayout}
-        form={form}
-        name="register"
-        onFinish={onFinish}
-        scrollToFirstError
-      >
-        <Form.Item
-          name="username"
-          label="Username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Username!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+    <div className="bg-login">
+      <Row className="login-screen">
+        <Col span={9}>
+          <div className="welcome">Welcome To</div>
+          <div className="name-game">
+            <div className="c">C</div>
+            <div className="a">A</div>
+            <div className="r">R</div>
+            <div className="o">O</div>
+            <div className="online">Online</div>
+          </div>
+          <div className="board">
+            <div>
+              <div className="xo">
+                <div className="x">X</div>
+                <div className="o">O</div>
+              </div>
+              <div className="xo">
+                <div className="o">O</div>
+                <div className="x">X</div>
+              </div>
+            </div>
+            <div className="loser">LOSER</div>
+            <div className="winner">WINNER</div>
+          </div>
+        </Col>
+        <Col span={9} className="form-register">
+          <div >
+            <h2 className="register">REGISTER</h2>
+            <Form
+              {...formItemLayout}
+              form={form}
+              className="register-form"
+              name="register"
+              onFinish={onFinish}
+              scrollToFirstError
+              // className="register-form"
+            >
+              <Form.Item
+                name="username"
+                label="Username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Username!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-          hasFeedback
-        >
-          <Input.Password />
-        </Form.Item>
+              <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+                hasFeedback
+              >
+                <Input.Password />
+              </Form.Item>
 
-        <Form.Item
-          name="confirm"
-          label="Confirm Password"
-          dependencies={["password"]}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: "Please confirm your password!",
-            },
-            ({ getFieldValue }) => ({
-              validator(rule, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
+              <Form.Item
+                name="confirm"
+                label="Confirm Password"
+                dependencies={["password"]}
+                hasFeedback
+                rules={[
+                  {
+                    required: true,
+                    message: "Please confirm your password!",
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(rule, value) {
+                      if (!value || getFieldValue("password") === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(
+                        "The two passwords that you entered do not match!"
+                      );
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                name="nickname"
+                label={
+                  <span>
+                    Nickname
+                    <Tooltip title="What do you want others to call you?">
+                      <QuestionCircleOutlined />
+                    </Tooltip>
+                  </span>
                 }
-                return Promise.reject(
-                  "The two passwords that you entered do not match!"
-                );
-              },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your nickname!",
+                    whitespace: true,
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">
+                  Register
+                  {/* <Link to ='/'></Link> */}
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+      </Row>
 
-        <Form.Item
-          name="nickname"
-          label={
-            <span>
-              Nickname
-              <Tooltip title="What do you want others to call you?">
-                <QuestionCircleOutlined />
-              </Tooltip>
-            </span>
-          }
-          rules={[
-            {
-              required: true,
-              message: "Please input your nickname!",
-              whitespace: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-            {/* <Link to ='/'></Link> */}
-          </Button>
-        </Form.Item>
-      </Form>
+      {/* <RegistrationForm/> */}
     </div>
   );
 };
--- src/pages/LoginScreen/LoginScreen.js
+++ src/pages/LoginScreen/LoginScreen.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import NormalLoginForm from '../../components/Login/Login';
-import RegistrationForm from '../../components/Register/Register';
-
-LoginScreen.propTypes = {
-    
-};
+import React from "react";
+import { Row, Col } from "antd";
+import NormalLoginForm from "../../components/Login/Login";
+import "./style.scss";
+LoginScreen.propTypes = {};
 
 function LoginScreen(props) {
-    return (
-        <div>
-            <NormalLoginForm/>
-            {/* <RegistrationForm/> */}
-        </div>
-    );
+  return (
+    <div className="bg-login">
+      <Row className="login-screen">
+        <Col span={9}>
+          <div className="welcome">Welcome To</div>
+          <div className="name-game">
+            <div className="c">C</div>
+            <div className="a">A</div>
+            <div className="r">R</div>
+            <div className="o">O</div>
+            <div className="online">Online</div>
+          </div>
+          <div className="board">
+              <div>
+            <div className="xo">
+              <div className="x">X</div>
+              <div className="o">O</div>
+            </div>
+            <div className="xo">
+              <div className="o">O</div>
+              <div className="x">X</div>
+            </div>
+            </div>
+            <div className="loser">LOSER</div>
+            <div className="winner">WINNER</div>
+          </div>
+        </Col>
+        <Col span={9} className="form-login">
+          <NormalLoginForm />
+        </Col>
+      </Row>
+
+      {/* <RegistrationForm/> */}
+    </div>
+  );
 }
 
 export default LoginScreen;
