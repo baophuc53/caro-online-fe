5426f71e3f79919c1a62ecf47b21c4bab16083de
VO THANH PHONG <vophong1612@gmail.com>
2020-12-09 10:25:42 +0700

    send token to sever when connect

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import { Button } from "antd";
+import Axios from "axios";
+import config from "../../config/config.json";
 const ENDPOINT = "http://localhost:8000";
 
-
 function Home() {
-
-  const [onlineUsers , setonlineUsers] = useState([]);
+  const [onlineUsers, setonlineUsers] = useState([]);
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("send-online-user-list", data => {
-      console.log(data);
-      setonlineUsers(data);
-    });
+    const token = localStorage.getItem("token");
+    if (token != null) {
+      Axios.post(`${config.dev.path}`, { token })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+          alert(err.message);
+        });
+      const socket = socketIOClient(ENDPOINT);
+      socket.on("send-online-user-list", (data) => {
+        console.log(data);
+        setonlineUsers(data);
+      });
+    }
   }, []);
 
   const Signout = (props) => (
@@ -29,16 +40,11 @@ function Home() {
 
   return (
     <div className="App">
-      
       {Signout()}
 
-        {onlineUsers.map((OLUser) => (
-          <div key = {OLUser.id}>
-            {OLUser.nickname}
-          </div>
-        ))}
-
-
+      {onlineUsers.map((OLUser) => (
+        <div key={OLUser.id}>{OLUser.nickname}</div>
+      ))}
     </div>
   );
 }
